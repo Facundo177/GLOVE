a445b1300f86124f30d46ea54dd0009f9f6f92f3
Alegarcia049 <alegarcia049@gmail.com>
2023-09-01 18:31:40 +0000

    up

---
+++ Config_puertos.ino
@@ -0,0 +1,23 @@
+#define PUL1 3     //Pulsador de a,b,c
+#define PUL2 4    //Pulsador de d,e,f
+#define PUL3 5     //Pulsador de g,h,i
+#define PUL4 6    //Pulsador de j,k,l
+#define PUL5 7    //Pulsador de m,n,ñ
+#define PUL6 8    //Pulsador de o,p,q
+#define PUL7 9    //Pulsador de r,s,t
+#define PUL8 10    //Pulsador de u,v,w
+#define PUL9 11    //Pulsador de x,y,z
+#define PUL10 12   //Pulsador de space/send
+#define PUL11 13   //Pulsador de erase/delete
+#define PUL12 15    //Pulsador configurable
+
+
+#define VIB1 37
+#define VIB2 36
+#define VIB3 33
+#define VIB4 31
+#define VIB5 30
+#define VIB6 29
+#define VIB7 28
+#define VIB8 27
+#define VIB9 26
